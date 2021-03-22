96a1a453dcbe28f6477f2345dc3f295f0f5e9c73
tanselbay1 <tanselbay1@hotmail.com>
2021-03-22 16:14:17 +0700

    Logic and UI added

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 let containerElement = document.getElementById('container');
+const clearButton = document.getElementById('clear');
 
+//Create Grids
 
 function gridMaker(size){
+    containerElement.innerHTML = '';
     let range = size * size;
+    containerElement.style.setProperty('--gridSize', size);
     for (let i = 1; i <= range; i++){
         let createAnotherGridDiv = document.createElement('div');
         containerElement.appendChild(createAnotherGridDiv);
     }
+    
 }
+gridMaker(25);
 
-gridMaker(16);
+//Drew Grids
 
-const drewBlack = function black(obj){
+function drewBlack(obj) {
     obj.target.classList.add('drew');
 }
 
 let grid = document.querySelectorAll('#container div');
+let gridEvent = Array.from(grid).map(box => box.addEventListener('mouseover', drewBlack));
+console.log(grid);
 
-Array.from(grid).map(box => box.addEventListener('mouseover', drewBlack));
+// Events
+
+function clearGrids(){
+    for (let i =0; i< grid.length; i++){
+        grid[i].classList.remove('drew');
+    }
+    let askSize = prompt("Please enter size of the grid-box between 1-40:");
+    while (askSize < 1 || askSize > 40 || isNaN(askSize)){
+        askSize = prompt("Please enter size of the grid-box between 1-40:");
+    }
+    gridMaker(askSize);
+    
+    grid = document.querySelectorAll('#container div');
+    gridEvent = Array.from(grid).map(box => box.addEventListener('mouseover', drewBlack));
+    console.log(grid);
+}
+
+clearButton.addEventListener('click', clearGrids);
